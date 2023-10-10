2eab245dc840a0e8ac6ed58ff97ff9d085a031d1
MatheusGoetz <matheusgoetz@gmail.com>
2023-10-10 14:36:15 -0300

    Projeto carrinho de compras finalizado paraticando JS

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,23 @@
+let valorTotal = 0;
+
+function adicionar() {
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorProduto = produto.split('R$')[1];
+    let qntdProduto = document.getElementById('quantidade').value;
+    let calculoPreco =  valorProduto * qntdProduto;
+
+    let listaCarrinho = document.getElementById('lista-produtos');
+    listaCarrinho.innerHTML = listaCarrinho.innerHTML + ` <section class="carrinho__produtos__produto">
+    <span class="texto-azul">${qntdProduto}X</span> ${nomeProduto} <span class="texto-azul">${valorProduto}</span>
+  </section>`
+  
+  valorTotal += calculoPreco
+
+    let totalCarrinho = document.getElementById('valor-total');
+    totalCarrinho.innerHTML = `<span class="texto-azul" id="valor-total">R$${valorTotal}</span>`
+}
+
+function limpar() {
+
+}
